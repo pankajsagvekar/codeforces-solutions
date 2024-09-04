e47ca8c7fbdecec0e056098659b51223bce7142a
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-04 19:55:43 +0530

    initial commit

---
+++ 800/Minimize_A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Watermelon_4A{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		if(t <= 55 && t > 0){
+			for(int i = 0; i < t; i++){
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				System.out.println(b-a);
+			}
+		}
+	}
+}
