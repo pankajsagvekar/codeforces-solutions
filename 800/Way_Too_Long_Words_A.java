38fff61d2bfab02e2af067d9cd8b002104ebbadd
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-05 01:36:23 +0530

    solution

---
+++ 800/Way_Too_Long_Words_A.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Way_Too_Long_Words_A{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		String[] words = new String[n];
+
+		for(int i = 0; i<words.length; i++){
+			words[i] = sc.next();
+		}
+
+		shortWords(words);
+	}
+
+	private static void shortWords(String[] words){
+		for(String word : words){
+			if (word.length() > 10){
+				int midString = word.length() -2;
+				System.out.println(word.substring(0, 1) +Integer.toString(midString)+ word.substring(word.length()-1));
+			}
+			else{
+				System.out.println(word);
+			}
+		}
+	}
+}
