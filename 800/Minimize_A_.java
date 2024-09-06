1a368b0150b106e06a9f19521c0c3800abfdf908
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-06 19:55:58 +0530

    edit

--- 800/Minimize_A_.java
+++ 800/Minimize_A_.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Watermelon_4A{
+public class Minimize_A_{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
