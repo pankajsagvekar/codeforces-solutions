56a9fe0cb3f3e60e867f2f46d21daf6b0854b2c7
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-15 17:52:03 +0530

    matrix solution implement my own algorithm

---
+++ 800/Beautiful_Matrix_A.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Beautiful_Matrix_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int matrix[][] = new int[5][5];
+        int row = 0, column = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = sc.nextInt();
+                
+                //get pos of 1
+                if (matrix[i][j] == 1){
+                    row = i;
+                    column = j;
+                }
+            }
+        }
+
+        int noOfMoves = Math.abs(column - 2) + Math.abs(row - 2); // 2 = middle position
+        System.out.println(noOfMoves);
+    }
+}
