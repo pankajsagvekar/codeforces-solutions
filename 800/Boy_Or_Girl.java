33051bd903d0c171aeee049d39fe88a016102404
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-18 01:49:16 +0530

    boy or girl solution

---
+++ 800/Boy_Or_Girl.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Boy_Or_Girl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        HashSet<Character> distinctWord = new HashSet<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            distinctWord.add(word.charAt(i));
+        }
+
+        if (distinctWord.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        }
+        else{
+            System.out.println("IGNORE HIM!");
+        }
+    }
+}
